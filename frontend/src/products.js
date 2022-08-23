25aa9dedde3746a738c7cab66e8d069e0ea2ec03
Pavan <pavan.b.m@accenture.com>
2022-08-23 19:00:11 +0530

    Home screen 8/23

---
+++ frontend/src/products.js
@@ -0,0 +1,81 @@
+const products = [
+  {
+    _id: 1,
+    name: 'Airpods Wireless Blueetooth Headphones',
+    image: '/images/airpods.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible deices wirelessly High quality AAC audio offers immersive listening experience',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    _id: 2,
+    name: 'iPhone 11 Pro 256GB Memory',
+    image: '/images/phone.jpg',
+    description:
+      'Introducing the iPhone 11 pro. A transformative triple camera system',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 599.99,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 4,
+  },
+  {
+    _id: 3,
+    name: 'Cannon EOS 80D DSLR Camera',
+    image: '/images/camera.jpg',
+    description:
+      'Characterized by versatile imaging specs, the Canons EOS 80D further clarifies itself using a pair of robust focusing  systmes',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 3,
+  },
+  {
+    _id: 4,
+    name: 'Sony playstation 4 pro white version',
+    image: '/images/playstation.jpg',
+    description:
+      'the ultimate home entertainment center starts with playstation, whether you are into games, HDmovies.',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 3,
+  },
+  {
+    _id: 5,
+    name: 'Logitech G-Series gaming mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Get a better handle with your game by syncing this logitech G-series gaming mouse it containes six programmabe buttons',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 2,
+  },
+  {
+    _id: 6,
+    name: 'Amazon Echo Dot 3rd Generation ',
+    image: '/images/alexa.jpg',
+    description: 'Meet Echo Dot-our most popular smart speaker',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 4,
+  },
+];
+
+export default products;
