f8a41d4c152a078e6a69b9528fc56d28557808f1
Pavan <pavan.b.m@accenture.com>
2022-09-17 18:58:31 +0530

    9/17 models in mongodb

---
+++ backend/models/userModel.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
