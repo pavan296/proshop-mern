be306fc83f1279ac2449da03d10ba2db6d68372b
Pavan Manjunath <pavanbm17@gmail.com>
2022-10-27 15:02:11 +0530

    redux and custom error handler

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,23 +1,22 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs'
 
-
-const users=[
-    {
-        name:'Admin User',
-        email:'admin@example.com',
-        password:bcrypt.hashSync('123456',10),
-        isAdmin:true
-    },
-    {
-        name:'Pavan',
-        email:'pavan@example.com',
-        password:bcrypt.hashSync('123456',10),
-    },
-    {
-        name:'Namratha',
-        email:'namratha@example.com',
-        password:bcrypt.hashSync('123456',10),
-    }
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
 ]
 
-export default users;
+export default users
